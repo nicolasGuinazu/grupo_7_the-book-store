b4ef34ae41e86dabeca8249819f268c94d0c098e
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-06-05 13:53:03 -0300

    se agrega la ruta para el registerForm

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/registerForm.html'))
+})
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
