c71ad25c55a24d9b2f7683fc76aaac83916d677e
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-22 18:06:08 -0300

    agrego middlewares para manipular datos del form, quito body parser (deprecated)

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const path=require('path')
 const methodOverride = require('method-override')
-const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 /****************** PUBLIC PATH *************************/
 app.use(express.static(path.join(__dirname, 'public')));
