9b2f307e6b0e7dc7eb134e882d67fe96a42b624f
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-01 10:02:44 -0300

    se agrega logica de guardar en session la info,si existe, de las cookies

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,5 @@
 function authMiddleware(req,res,next){
-    
-    if(req.session.loggedUser == undefined){
+    if(!req.session.loggedUser){   //si no hay nada en session es undefined. Si pasa esto entra en el if y redirecciona
         return res.redirect('/users/login')
     }else{
    
