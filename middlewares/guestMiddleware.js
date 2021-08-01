4684022dd6a3e03520d6f5e7d3cbb792bdac6a9e
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-31 21:07:28 -0300

    middleware para usuarios loggeados

--- middlewares/guestMiddleware.js
+++ middlewares/guestMiddleware.js
@@ -1,3 +1,9 @@
-module.exports={
-    
+function authMiddleware(req,res,next){
+    if(req.session.loggedUser == undefined){
+        next();
+    }else{
+        return res.redirect('/')
+    }
 }
+
+module.exports=authMiddleware;
