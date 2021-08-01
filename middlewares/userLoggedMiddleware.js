837a5e8b2dfaafadd739ee70b26f3cac3ff70c92
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-01 10:04:27 -0300

    se agrega logica de guardar en session la info,si existe, de las cookies

--- middlewares/userLoggedMiddleware.js
+++ middlewares/userLoggedMiddleware.js
@@ -1,15 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+
 function userLoggedMiddleware(req,res,next){
     res.locals.logged=false;
-
+    let cookieExists
+    if(req.cookies && req.cookies.userEmail){ //que haya cookies (evitar error property of undefined) y que exista esa
+       cookieExists=req.cookies.userEmail;
+    }
+    let userExists= users.find( elem => elem.email == cookieExists); //buscar en db usuario con email de esa cookie
+    if(cookieExists && userExists){
+        req.session.loggedUser=userExists // si existe pasarlo a session
+    }
     if(req.session.loggedUser){
-        
         res.locals.logged=true;
-        res.locals.loggedUser=req.session.loggedUser;
-        console.log(res.locals.loggedUser)
-        
+        res.locals.loggedUser=req.session.loggedUser;     
     };
-
-    console.log('estoy en auth')
     next();
 }
 
