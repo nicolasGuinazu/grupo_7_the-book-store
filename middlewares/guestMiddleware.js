ab6fe1252114ce28d9591bba54d2db93ecae7742
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-02 13:08:46 -0300

    redireccion al profile del usuario logeado si quiere acceder a rutas de guests

--- middlewares/guestMiddleware.js
+++ middlewares/guestMiddleware.js
@@ -2,7 +2,7 @@ function guestMiddleware(req,res,next){
     if(req.session.loggedUser == undefined){
         next();
     }else{
-        return res.redirect('/')
+        return res.redirect('/users/profile')
     }
 }
 
