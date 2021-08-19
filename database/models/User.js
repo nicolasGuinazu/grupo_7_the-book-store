5669c67943f72d356ef6bc606cdce2f1a87a12f7
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-19 14:57:59 -0300

    modelo para tabla users, para esta y el resto falta agregar relaciones

---
+++ database/models/User.js
@@ -0,0 +1,49 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'User';
+    let cols = {
+        iduser: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        admin: {
+            type: dataTypes.CHAR(1)  //no esta binary
+        },
+        email: {
+            type: dataTypes.VARCHAR(100)
+        },
+        password: {
+            type: dataTypes.VARCHAR(100)
+        },
+        avatar: {
+            type: dataTypes.VARCHAR(64)
+        },
+        user_name: {
+            type: dataTypes.VARCHAR(64)
+        },
+        name: {
+            type: dataTypes.VARCHAR(32)
+        }
+        ,
+        last_name: {
+            type: dataTypes.VARCHAR(32)
+        },
+        birth_date: {
+            type: dataTypes.DATE
+        },
+        phone_number: {  
+            type: dataTypes.INTEGER
+        }
+    };
+    let config = {
+        tableName: 'users',
+        timestamps: false
+    };
+    const User = sequelize.define(alias, cols, config)
+
+    /*  User.associate=function(models){
+       
+    } */ 
+
+    return User
+}
