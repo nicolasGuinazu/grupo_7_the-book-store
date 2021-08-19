28d64b16beb4751ba17313cad14a3df474347b09
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-19 11:26:25 -0300

    archivo creado por sequelize, agrego module exports en config e inicio con modelo genres

---
+++ database/models/Genre.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Genre';
+    let cols = {
+        idgenre: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: dataTypes.STRING
+        }
+    };
+    let config = {
+        tableName: 'genres',
+        timestamps: false
+    };
+    const Genre = sequelize.define(alias, cols, config)
+
+    /*  Genre.associate=function(models){
+        Genre.hasMany(models.Products,{
+            as:'products',
+            foreignKey:'genre_id'
+        })
+    } */ 
+
+    return Genre
+}
