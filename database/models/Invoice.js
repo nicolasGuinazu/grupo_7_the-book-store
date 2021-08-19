22718a8fe834c652328978643c62b5a01e5309c3
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-19 14:57:12 -0300

    modelo para tabla invoices

---
+++ database/models/Invoice.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Invoice';
+    let cols = {
+        idinvoice: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        amount: {
+            type: dataTypes.DECIMAL(10,2)
+        }
+        
+    };
+    let config = {
+        tableName: 'invoices',
+        timestamps: false
+    };
+    const Invoice = sequelize.define(alias, cols, config)
+
+    /*  Invoice.associate=function(models){
+        )
+    } */ 
+
+    return Invoice
+}
