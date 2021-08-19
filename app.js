183d829e82af30a35d95e52a2e8e8f107e9549d8
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-19 11:26:48 -0300

    require del dotenv para poder usar variables de entorno

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path=require('path');
+require('dotenv').config()
 const methodOverride = require('method-override');
 const session=require('express-session');
 const cookieParser=require('cookie-parser');
