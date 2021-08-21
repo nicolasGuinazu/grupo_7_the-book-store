fb574c5371c818de5d7f65e44919233c811ca039
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-21 17:43:10 -0300

    incorporacion de middlewares a las rutas de create,modify y delete

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const authMiddleware=require('../middlewares/authMiddleware')
-
+const adminMiddleware=require('../middlewares/adminMiddleware')
 // ********** Manejo de archivos con Multer ***********
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -23,18 +23,18 @@ const productsController = require('../controllers/productsController');
 router.get('/', productsController.index);
 
 // ************ Crear Producto ***********
-router.get('/create', authMiddleware,productsController.create);
+router.get('/create', authMiddleware,adminMiddleware,productsController.create); //middlewares para checkear que este logeado y se admin
 router.post('/create', upload.single('image'), productsController.store);
 //router.post('/create', productsController.store);
 
 // ************ Modificar Producto ***********
 router.get('/modify/:id', authMiddleware,productsController.modify);
-router.put('/modify/:id',upload.single('image'), productsController.processModify);
+router.put('/modify/:id',adminMiddleware,upload.single('image'), productsController.processModify);
 
 // ************ Detalle de Producto ***********
 router.get('/detail/:id', productsController.detail);
 
 // ************ Elminar Producto ***********
-router.delete('/:id', productsController.destroy);
+router.delete('/:id', adminMiddleware,productsController.destroy);
 
 module.exports = router;
