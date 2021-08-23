f080c9b83cfb17319700a588b47f5afd787643c4
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-23 11:46:00 -0300

    incorporo nuevos metodos, incluyo asociaciones

--- controllers/testController.js
+++ controllers/testController.js
@@ -4,13 +4,54 @@ const db = require('../database/models');
 const controller = {
     genres: async (req, res) => {
         try{
-            let genres=await db.Genre.findAll()
+            let genres=await db.Genre.findAll({
+                include: ['products']
+            })
             res.send(genres)
         }catch(err){
             console.log(err)
         }
   
     },
+    users: async (req, res) => {
+        try{
+            let users=await db.User.findAll()
+            res.send(users)
+        }catch(err){
+            console.log(err)
+        }
+  
+    },
+    paymentMethods: async (req, res) => {
+        try{
+            let paymentMethod=await db.PaymentMethod.findAll()
+            res.send(paymentMethod)
+        }catch(err){
+            console.log(err)
+        }
+  
+    },
+    products: async (req, res) => {
+        try{
+            let products=await db.Product.findAll() //Association with alias "author" does not exist on Product
+            res.send(products)
+        }catch(err){
+            console.log(err)
+        }
+  
+    },
+    authors: async (req, res) => {
+        try{
+            let authors=await db.Author.findAll({
+                include: ['products']
+            })
+            res.send(authors)
+        }catch(err){
+            console.log(err)
+        }
+  
+    },
+
   
 }
 
