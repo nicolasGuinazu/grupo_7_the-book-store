8121392cd74dc16f348bc4c6ad9e3ce9bff062b4
PiaTcq <mertrebucq@gmail.com>
2021-08-24 00:34:31 -0300

    agregue controlador invoices

--- controllers/testController.js
+++ controllers/testController.js
@@ -20,58 +20,6 @@ const controller = {
             console.log(err)
         }
     },
-    genres: async (req, res) => {
-        try{
-            let genres=await db.Genre.findAll({
-                include: ['products']
-            })
-            res.send(genres)
-        }catch(err){
-            console.log(err)
-        }
-  
-    },
-    users: async (req, res) => {
-        try{
-            let users=await db.User.findAll()
-            res.send(users)
-        }catch(err){
-            console.log(err)
-        }
-  
-    },
-    paymentMethods: async (req, res) => {
-        try{
-            let paymentMethod=await db.PaymentMethod.findAll()
-            res.send(paymentMethod)
-        }catch(err){
-            console.log(err)
-        }
-  
-    },
-    products: async (req, res) => {
-        try{
-            let products=await db.Product.findAll({
-                include:['author','genre']
-            }) //Association with alias "author" does not exist on Product
-            res.send(products)
-        }catch(err){
-            console.log(err)
-        }
-  
-    },
-
-    editorials: async (req, res) => {
-        try{
-            let editorials=await db.Editorial.findAll({
-                include: ['products']
-            })
-            res.send(editorials)
-        }catch(err){
-            console.log(err)
-        }
-    },
-
     carts: async (req, res) => {
         try{
             let carts=await db.Cart.findAll({
@@ -82,7 +30,61 @@ const controller = {
             console.log(err)
         }
     },
-  
+    editorials: async (req, res) => {
+        try{
+            let editorials=await db.Editorial.findAll({
+                include: ['products']
+            })
+            res.send(editorials)
+        }catch(err){
+            console.log(err)
+        }
+    },
+    genres: async (req, res) => {
+        try{
+            let genres=await db.Genre.findAll({
+                include: ['products']
+            })
+            res.send(genres)
+        }catch(err){
+            console.log(err)
+        }
+    },
+    /*invoices: async (req, res) => {
+        try{
+            let invoice=await db.Invoice.findAll()
+            res.send(invoice)
+        }catch(err){
+            console.log(err)
+        }    
+    },*/
+    paymentMethods: async (req, res) => {
+        try{
+            let paymentMethod=await db.PaymentMethod.findAll()
+            res.send(paymentMethod)
+        }catch(err){
+            console.log(err)
+        }
+    },
+    products: async (req, res) => {
+        try{
+            let products=await db.Product.findAll({
+                include:['author','genre']
+            }) //Association with alias "author" does not exist on Product
+            res.send(products)
+        }catch(err){
+            console.log(err)
+        }
+        
+    },
+    users: async (req, res) => {
+        try{
+            let users=await db.User.findAll()
+            res.send(users)
+        }catch(err){
+            console.log(err)
+        }
+    },  
 }
 
 module.exports = controller;
