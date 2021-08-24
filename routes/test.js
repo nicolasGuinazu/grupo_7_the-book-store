61bab345ed6b9693510aa53e99d42a861cddc96a
PiaTcq <mertrebucq@gmail.com>
2021-08-24 00:34:49 -0300

    agregue ruta a invoices

--- routes/test.js
+++ routes/test.js
@@ -8,9 +8,10 @@ router.get('/authors', testController.authors);
 router.get('/carts', testController.carts);
 router.get('/editorials', testController.editorials);
 router.get('/genres', testController.genres);
-router.get('/users', testController.users);
-router.get('/products', testController.products);
+/*router.get('/invoices', testController.invoices);*/
 router.get('/payment-method', testController.paymentMethods);
+router.get('/products', testController.products);
+router.get('/users', testController.users);
 
 /* ; 
 router.get('/products', testController.index); 
