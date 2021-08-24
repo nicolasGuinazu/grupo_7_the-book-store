d429c52fc98aea5b4569d7494d190ef535ff133f
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-24 18:37:11 -0300

    rutas para el crud

--- routes/test.js
+++ routes/test.js
@@ -13,8 +13,12 @@ router.get('/payment-method', testController.paymentMethods);
 router.get('/products', testController.products);
 router.get('/users', testController.users);
 
-/* ; 
-router.get('/products', testController.index); 
-*/
+//rutas para el crud de producto
+router.get('/products', testController.products);//todos los productos
+router.get('/products/:id', testController.singleProduct); //detalle del producto// igual para el get del modify
+router.post('/products/create', testController.create);  //creacion de producto
+router.put('/products/modify/:id', testController.processModify)//metodo update (modificacion de base) falta la ruta por get
+router.delete('/products/delete/:id', testController.destroy); //metodo destroy (borrado de base) falta la ruta por get
+
 
 module.exports = router;
