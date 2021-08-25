d63625262340bcdc06334c42d14ea74fa99a133a
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-25 11:07:12 -0300

    se incorporan rutas para crud de producto

--- routes/test.js
+++ routes/test.js
@@ -14,11 +14,13 @@ router.get('/products', testController.products);
 router.get('/users', testController.users);
 
 //rutas para el crud de producto
-router.get('/products', testController.products);//todos los productos
-router.get('/products/:id', testController.singleProduct); //detalle del producto// igual para el get del modify
-router.post('/products/create', testController.create);  //creacion de producto
-router.put('/products/modify/:id', testController.processModify)//metodo update (modificacion de base) falta la ruta por get
-router.delete('/products/delete/:id', testController.destroy); //metodo destroy (borrado de base) falta la ruta por get
+router.get('/products/list', testController.index);//trae el listado con todos los productos
+router.get('/products/create', testController.create); //vista con formulario creacion
+router.post('/products/create', testController.store);  //creacion de producto
+router.get('/products/details/:id', testController.singleProduct); //detalle de un producto
+router.get('/products/modify/:id',testController.modify); //vista con formulario edicion
+router.put('/products/modify/:id', testController.processModify)//metodo update (modificacion de base) 
+router.delete('/products/delete/:id', testController.destroy); //metodo destroy (hard delete) 
 
 
 module.exports = router;
