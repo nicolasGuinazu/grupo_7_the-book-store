a6c73ada832f6f3711b8a8744eb64054561fe92b
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-29 20:46:07 -0300

    agrego subcarpeta de products en la ruta de multer

--- routes/products.js
+++ routes/products.js
@@ -7,7 +7,7 @@ const adminMiddleware=require('../middlewares/adminMiddleware')
 // ********** Manejo de archivos con Multer ***********
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/images')
+        cb(null, './public/images/products')
     },
     filename: function (req, file, cb) {      
         cb(null, file.fieldname + '-' + Date.now() + '.png')
