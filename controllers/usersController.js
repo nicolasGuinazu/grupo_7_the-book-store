baa71598ebaa9d5dd17a5b60742589d41c8b4723
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-29 20:44:39 -0300

    comento codigo utilizado en la json db

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+/* const fs = require('fs');
 const path = require('path');
-const bcrypt = require('bcryptjs');
-const db = require("../database/models");
-
 const usersFilePath = path.join(__dirname, '../data/users.json');
-const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')); */
+const db = require("../database/models");
+const bcrypt = require('bcryptjs');
+
 
 const controller = {
     login: (req, res) =>{
