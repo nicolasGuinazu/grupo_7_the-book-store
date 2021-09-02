9730f28d5fc1d8c46b2b178beed66413e7c7d93d
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-09-02 18:26:20 -0300

    comienzo agregando validaciones custom

--- public/js/validations.js
+++ public/js/validations.js
@@ -1,5 +1,40 @@
-let email=document.querySelector('#email')
-email.addEventListener('keypress',()=>{
-    console.log(email.value); 
+window.addEventListener('load',function(){
+    let emailError=false;
+    let passwordError=false;
+    let button=document.querySelector('#button-submit');
+    let form=document.querySelector('form');
+    let email=document.querySelector('#email');
+    let password=document.querySelector('#password');
+    email.addEventListener('change',function(){
+        let inputValue=this.value;
+        if(inputValue.length == ""){
+            email.style.border="red 1px solid";
+            emailError=true
+            button.style.backgroundColor="grey";
+            button.style.disabled;
+            button.style.cursor='not-allowed';
+        }else{
+            email.style.border="black 1px solid";
+            
+        }
+    })
+    
+    password.addEventListener('change',function(){
+        let inputValue=this.value;
+        if(inputValue.length == ""){
+            password.style.border="red 1px solid"
+            passwordError=true
+        }else{
+            password.style.border="black 1px solid";
+        }
+    })
+
+    form.addEventListener('submit',function(event){
+        if(emailError || passwordError){
+            event.preventDefault();
+        }
+        
+        
+    })
 })
 
