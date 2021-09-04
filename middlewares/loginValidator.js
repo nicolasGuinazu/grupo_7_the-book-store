4b437afe8566cc627373e7ccc41197a45f25ad23
Marcos Cardoso <cardoso.marcosn@gmail.com>
2021-09-04 14:47:17 -0300

    Corrección ortográfica loginValidator.js

--- middlewares/loginValidator.js
+++ middlewares/loginValidator.js
@@ -1,7 +1,7 @@
 const {body}=require('express-validator')
 
 module.exports=[
-    body('email').notEmpty().withMessage('Debes escribir un email valido'),
-    body('password').notEmpty().withMessage('Debes crear una contrasena')
+    body('email').notEmpty().withMessage('Debes escribir un email válido'),
+    body('password').notEmpty().withMessage('Debes ingresar una contraseña')
 ]
 
