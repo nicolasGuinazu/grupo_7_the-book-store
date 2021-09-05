2e763ef86933e91ebf1a7ab8c63fc04ca0d5febb
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-09-05 00:47:19 -0300

    agrego mensajes de error para pass y email

--- public/js/validations.js
+++ public/js/validations.js
@@ -5,27 +5,34 @@ window.addEventListener('load',function(){
     let form=document.querySelector('form');
     let email=document.querySelector('#email');
     let password=document.querySelector('#password');
+    let errorMessageEmail=document.querySelector('#error-mail');
+    let errorMessagePass=document.querySelector('#error-pass');
+    let body=document.querySelector('body');
+
     email.addEventListener('change',function(){
         let inputValue=this.value;
-        if(inputValue.length == ""){
+        if(inputValue.trim() == "" || !inputValue.includes("@")){
             email.style.border="red 1px solid";
+            errorMessageEmail.innerText=`Debes introducir un email`
             emailError=true
             button.style.backgroundColor="grey";
             button.style.disabled;
             button.style.cursor='not-allowed';
         }else{
             email.style.border="black 1px solid";
-            
+            errorMessageEmail.innerText=""
         }
     })
     
     password.addEventListener('change',function(){
         let inputValue=this.value;
-        if(inputValue.length == ""){
+        if(inputValue.trim() == ""){
+            errorMessagePass.innerText=`Debes introducir un password`
             password.style.border="red 1px solid"
             passwordError=true
         }else{
             password.style.border="black 1px solid";
+            errorMessagePass.innerText=''
         }
     })
 
