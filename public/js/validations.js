2ca06525a3852bac29c30b9b8d3799571c00b101
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-09-11 13:16:58 -0300

    modifico estilos a agregar en validaciones

--- public/js/validations.js
+++ public/js/validations.js
@@ -7,7 +7,6 @@ window.addEventListener('load',function(){
     let password=document.querySelector('#password');
     let errorMessageEmail=document.querySelector('#error-mail');
     let errorMessagePass=document.querySelector('#error-pass');
-    let body=document.querySelector('body');
 
     email.addEventListener('change',function(){
         let inputValue=this.value;
@@ -16,11 +15,11 @@ window.addEventListener('load',function(){
             errorMessageEmail.innerText=`Debes introducir un email`
             emailError=true
             button.style.backgroundColor="grey";
-            button.style.disabled;
             button.style.cursor='not-allowed';
         }else{
             email.style.border="black 1px solid";
             errorMessageEmail.innerText=""
+            button.style=button.style
         }
     })
     
@@ -30,9 +29,12 @@ window.addEventListener('load',function(){
             errorMessagePass.innerText=`Debes introducir un password`
             password.style.border="red 1px solid"
             passwordError=true
+            button.style.backgroundColor="grey";
+            button.style.cursor='not-allowed';
         }else{
             password.style.border="black 1px solid";
             errorMessagePass.innerText=''
+            button.style=button.style
         }
     })
 
