78e816b3c4f7250b4cb24489d9a1573375b71602
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-09-12 13:15:28 -0300

    agrego funcionalidad de muestra de productos y borrado

--- public/js/carrito-display.js
+++ public/js/carrito-display.js
@@ -1,6 +1,18 @@
-window.addEventListener('load',function(){
-let cartcontainer=document.querySelector('#cart-container');
-let cartArray=JSON.parse(localStorage.getItem('product'))
-console.log(cartArray[0])
+window.addEventListener("load", function () {
+  let cartcontainer = document.querySelector("#cart-container");
+  let cartArray = JSON.parse(localStorage.getItem("product"));
 
-})
+  for (i = 0; i < cartArray.length; i++) {
+    cartcontainer.innerHTML += `<div class="cart-card"><p>${cartArray[i].title}</p><p>${cartArray[i].author}</p><p>${cartArray[i].price}</p><button class="erase">X</button></div>`;
+  }
+  for (i = 0; i < cartcontainer.children.length; i++) {
+    cartcontainer.children[i].addEventListener("click", (event) => {
+      console.log(event.target.parentElement.children[0].innerText);
+      let newcart = cartArray.filter(
+        (el) => el.title != event.target.parentElement.children[0].innerText
+      );
+      localStorage.setItem("product", JSON.stringify(newcart));
+      location.reload();
+    });
+  }
+});
