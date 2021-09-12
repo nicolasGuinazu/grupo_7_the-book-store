89d3f33f8797053626c1e9dcfbe7d1b01f3ea919
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-09-12 13:44:13 -0300

    comento codigo para mejorar interpretacion

--- public/js/carrito-display.js
+++ public/js/carrito-display.js
@@ -2,17 +2,16 @@ window.addEventListener("load", function () {
   let cartcontainer = document.querySelector("#cart-container");
   let cartArray = JSON.parse(localStorage.getItem("product"));
 
-  for (i = 0; i < cartArray.length; i++) {
+  for (i = 0; i < cartArray.length; i++) { //renderiza cada elemento del carrito en el DOM 
     cartcontainer.innerHTML += `<div class="cart-card"><p>${cartArray[i].title}</p><p>${cartArray[i].author}</p><p>${cartArray[i].price}</p><button class="erase">X</button></div>`;
   }
-  for (i = 0; i < cartcontainer.children.length; i++) {
-    cartcontainer.children[i].addEventListener("click", (event) => {
-      console.log(event.target.parentElement.children[0].innerText);
-      let newcart = cartArray.filter(
+  for (i = 0; i < cartcontainer.children.length; i++) { //cartcontainer (div que contiene los productos) es elemento padre, se accede a los children
+    cartcontainer.children[i].addEventListener("click", (event) => { //a cada child se agrega un eventlistener cuando se hace click
+      let newcart = cartArray.filter(  //nuevo array con todos los elementos menos el clickeado(el que se borra)
         (el) => el.title != event.target.parentElement.children[0].innerText
       );
       localStorage.setItem("product", JSON.stringify(newcart));
-      location.reload();
+      location.reload(); //recarga la pagina para poder ver la actualizacion del DOM
     });
   }
 });
--- public/js/carrito.js
+++ public/js/carrito.js
@@ -1,29 +1,29 @@
 
 window.addEventListener('load',function(){
 let boton = document.querySelector('#agregarcarrito');
-let product = document.querySelector('.datos-producto').querySelectorAll('h3')
+let product = document.querySelector('.datos-producto').querySelectorAll('h3') //cada h3 representa un dato del producto
 
 boton.addEventListener('click',function(){
-    let cart=[]
-    if(!localStorage.getItem('product')){
+    let cart=[] 
+    if(!localStorage.getItem('product')){ //si no hay nada en local storage se guarda cart (array vacio)
         localStorage.setItem('product',cart)
     }else{
-      cart=JSON.parse(localStorage.getItem('product'))
+      cart=JSON.parse(localStorage.getItem('product')) // parseo array existente en localstorage
     }
-    let purchasedProduct={
-        title:product[0].innerHTML,
+    let purchasedProduct={  //producto a guardarse en el carrito
+        title:product[0].innerHTML, //al seleccionarse todos los h3 se acceden como un array
         author:product[1].innerHTML,
         price:product[3].innerHTML,
     }
     
-    if(cart.find((el)=>el.title==product[0].innerHTML)){
-        console.log(cart.length)
-        console.log('ya esta')
+    if(cart.find((el)=>el.title==product[0].innerHTML)){ //si el elemento ya esta en el carrito no se agrega
+       
+        console.log('ya esta esta en el carrito! no se agrega')
         
     }else{
         cart.push(purchasedProduct)
     }
-    localStorage.setItem('product',JSON.stringify(cart))
+    localStorage.setItem('product',JSON.stringify(cart)) //se guarda en localstorage el array nuevamente
     
     
 })
