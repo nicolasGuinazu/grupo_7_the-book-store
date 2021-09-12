ac96adf9b3feaad759b8bf0282dbb1ec462ee94a
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-09-12 19:41:56 -0300

    agrego mensaje si no hay elementos en carrito e imagen

--- public/js/carrito-display.js
+++ public/js/carrito-display.js
@@ -1,9 +1,16 @@
 window.addEventListener("load", function () {
   let cartcontainer = document.querySelector("#cart-container");
   let cartArray = JSON.parse(localStorage.getItem("product"));
+  let buybutton = document.querySelector(".buy");
+  let carritoVacio=document.querySelector("#carrito-vacio");
 
+  if(cartArray.length<1){ //si el array del local storage esta vacio se muestra ese parrafo y se esconde boton comprar
+    carritoVacio.innerText='Tu carrito esta vacio!'
+    buybutton.classList.remove('buy');
+    buybutton.style.display='none'
+  }
   for (i = 0; i < cartArray.length; i++) { //renderiza cada elemento del carrito en el DOM 
-    cartcontainer.innerHTML += `<div class="cart-card"><p>${cartArray[i].title}</p><p>${cartArray[i].author}</p><p>${cartArray[i].price}</p><button class="erase">X</button></div>`;
+    cartcontainer.innerHTML += `<div class="cart-card"><p>${cartArray[i].title}</p><p>${cartArray[i].author}</p><p>${cartArray[i].price}</p><img style="height:100px; width:100px"src=${cartArray[i].image}><button class="erase">X</button></div>`;
   }
   for (i = 0; i < cartcontainer.children.length; i++) { //cartcontainer (div que contiene los productos) es elemento padre, se accede a los children
     cartcontainer.children[i].addEventListener("click", (event) => { //a cada child se agrega un eventlistener cuando se hace click
