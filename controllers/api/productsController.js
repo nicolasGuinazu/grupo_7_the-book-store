8341ea4148f8bf2647d738995201dd7979f0ebd7
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-09-16 19:02:40 -0300

    agrego logica para enviar countByGenre

--- controllers/api/productsController.js
+++ controllers/api/productsController.js
@@ -1,4 +1,5 @@
 const db = require("../../database/models");
+const { Sequelize } = require('sequelize');
 const controller={
     products: async (req, res) => {
         try {
@@ -6,10 +7,28 @@ const controller={
             include: ["author", "genre"],
             attributes: ['idproduct','name','synopsis'] //selecciona unicamente estas columnas de la base
           }); 
+          let genreList=[] //array base para guardar generos con productos asociados
+          let genres = await db.Genre.findAll({
+            attributes: { 
+              include: [[Sequelize.fn("COUNT", Sequelize.col("genre_id")), "count"]]  //cuenta cantidad de veces q se repite genre_id
+          },
+          include: ["products"],
+          group: ['idgenre'], //agrupa por idgenre
+          
+          });
+          for(let i = 0; i < genres.length; i++){
+           if(genres[i].dataValues.count>0){ //si el genero tiene al menos un producto se agrega al array (para no guardar generos sin productos asociados)
+            genreList.push({name:genres[i].dataValues.name,count:genres[i].dataValues.count})
+           }
+            
+         /*    newGenre.push({name:genres[i].name,count:genres[i].count}) */
+          }
+          
           res.json({
             meta:{
               status:200,
-              count:products.length
+              count:products.length,
+              countByCategory:genreList
             },
             data:{
               products,
