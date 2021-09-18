9116d3c9f701df9592263a0519a7c65d8f2fd781
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-09-18 11:39:33 -0300

    ruta para detalle de producto

--- routes/api/products.js
+++ routes/api/products.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productsController = require('../../controllers/api/productsController')
 
 router.get('/', productsController.products);  // localhost:3000/api/products || Devuelve listado completo de productos (id,name,synopsis)
+router.get('/:id',productsController.productsDetails) // localhost:3000/api/products/id
 
 module.exports = router;
