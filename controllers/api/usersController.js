8a5c7208170f26305464ddadd9b1ba0d552b7901
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-09-18 11:41:05 -0300

    metodo para retornar detalle del usuario y se agrega link de detalle al array de users

--- controllers/api/usersController.js
+++ controllers/api/usersController.js
@@ -5,20 +5,53 @@ const controller={
           let users = await db.User.findAll({
             attributes: ['iduser','name','email']
           });
+          let usersApi=users.map((user)=>{  //por cada elemento del array usuario se le agrega la propiedad urlDetail
+            
+            return{
+              ...user.dataValues, //la propiedad dataValues tiene todos los valores de ese user
+              urlDetail:`http://localhost:3000/api/users/${user.iduser}` //se agrega dinamicamente el id
+            } 
+          })
           res.json({
             meta:{
               status:200,
               count:users.length
             },
             data:{
-              users,
+              usersApi,
+           
             }
           });
+         
         } catch (err) {
           console.log(err);
         }
       },
+      usersDetails:async(req,res)=>{
+        try{
+          let singleUser=await db.User.findOne({
+            where:{iduser:req.params.id},
+            attributes: ['iduser','name','last_name','user_name','birth_date','phone_number','email','avatar']
+          })
+          res.json({
+            meta:{
+              status:200,
+            },
+            data:{
+              ...singleUser.dataValues,
+              urlAvatar:`http://localhost:3000/images/users/${singleUser.dataValues.avatar}`
+            }
+          });
+        }
+        catch(err){
+          console.log(err);
+        }
+
+      },
     
 }
+
+
+
 
 module.exports=controller
