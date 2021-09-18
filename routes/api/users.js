539fee6a370712db60f740a205a366107e44ef56
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-09-18 11:39:12 -0300

    ruta para detalle de usuario

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const usersController = require('../../controllers/api/usersController')
 
 router.get('/', usersController.users); // localhost:3000/api/users  || Devuelve listado completo de productos (id,name,email)
-
+router.get('/:id',usersController.usersDetails) // localhost:3000/api/users/id
 
 module.exports = router;
