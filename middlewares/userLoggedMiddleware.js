fe1f4f9c9eee58dcf0d64820d457faf20d58b54b
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-09-19 13:42:19 -0300

    reemplazo logica jsondb por la de la base de datos

--- middlewares/userLoggedMiddleware.js
+++ middlewares/userLoggedMiddleware.js
@@ -1,24 +1,27 @@
-const path = require('path');
-const fs = require('fs');
-const usersFilePath = path.join(__dirname, '../data/users.json');
-const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
-
+const db = require("../database/models")
 
 function userLoggedMiddleware(req,res,next){
     res.locals.logged=false;
-    let cookieExists
+    let cookieExists=''
     if(req.cookies && req.cookies.userEmail){ //que haya cookies (evitar error property of undefined) y que exista esa
        cookieExists=req.cookies.userEmail;
     }
-    let userExists= users.find( elem => elem.email == cookieExists); //buscar en db usuario con email de esa cookie
-    if(cookieExists && userExists){
-        req.session.loggedUser=userExists // si existe pasarlo a session
-    }
-    if(req.session.loggedUser){
-        res.locals.logged=true;
-        res.locals.loggedUser=req.session.loggedUser;     
-    };
-    next();
+    userExists=db.User.findOne({
+        where:{email:cookieExists},
+      }).then((user)=>{
+         
+        if(cookieExists && user){
+            req.session.loggedUser=user
+        }
+        if(req.session.loggedUser){
+            res.locals.logged=true;
+            res.locals.loggedUser=req.session.loggedUser;     
+        };
+        next();
+      }).catch(err=>console.log(err))
+      
+  
 }
 
 module.exports=userLoggedMiddleware;
+
