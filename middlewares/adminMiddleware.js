9b4110220cda997ffdbc5af03b31eb14ffebdb8c
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-10-06 16:09:23 -0300

    agrego un typo, agrego validacion para eliminar prodcutos desde un fetch y agrego logica de consultar si hay usuario en session

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,9 +1,10 @@
-function adminhMiddleware(req,res,next){
-    if(req.session.loggedUser.admin=='a'){   //si es admin sigue 
+function adminMiddleware(req,res,next){
+    console.log(req.headers)
+    if((req.headers.auth=='admin') || (req.session.loggedUser && req.session.loggedUser.admin=='a')){   //si es admin sigue
         next()
     }else{
         return res.redirect('/') //si es usuario se redirecciona al home
     }
 }
 
-module.exports=adminhMiddleware;
+module.exports=adminMiddleware;
