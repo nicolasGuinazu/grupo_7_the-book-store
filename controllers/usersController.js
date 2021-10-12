bdfa5744a4d50e20126a797e1d7f859d272d7812
nicolasGuinazu <nicolasguinazu1@gmail.com>
2021-10-12 13:42:48 -0300

    fix que permita logear automaticamente al usuario luego de registrado

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,3 @@
-/* const fs = require('fs');
-const path = require('path');
-const usersFilePath = path.join(__dirname, '../data/users.json');
-const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')); */
 const db = require("../database/models");
 const bcrypt = require('bcryptjs');
 const { validationResult} = require('express-validator');
@@ -33,20 +29,20 @@ const controller = {
                 email: req.body.email
                 }
                 });
-            
+
             if (userCheck){
                 let errorRepeat = "Existe un usuario registrado con este Email"
                 return res.render('./users/registerForm',{errorRepeat, old: req.body})
             }
 
         }catch (err) {
-            console.log(err);      
+            console.log(err);
             }
-        
+
 
          //Encrypt password
          let cryptedPass = bcrypt.hashSync(req.body.password, 10);
-    
+
          let userToCreate = {
            admin: "a",
            email: req.body.email,
@@ -58,51 +54,16 @@ const controller = {
            birth_date: req.body.birth_date,
            phone_number: req.body.phone_number
          }
-   
-         //console.log(userToCreate);
+
         try {
-          await db.User.create(userToCreate);
-          return  res.redirect('/users/login');
-    
+          let newUser=await db.User.create(userToCreate);
+          req.session.loggedUser=newUser
+          return  res.redirect('/');
+
         } catch (err) {
           console.log(err);
         }
-      }, 
-
-    /*registerUser: (req, res) =>{
-
-       //Obtengo la última posición del JSON para luego obtener el último ID
-       const lastUser = users[users.length - 1];
-
-       //Usuario a crear. Obtengo los datos ingresados en el form
-       const userToCreate = req.body;
-       const imageName = req.file.filename;
-
-       //Encriptamos la contraseña
-       let passEncriptada = bcrypt.hashSync(userToCreate.psw, 10);
-
-       //Armo objeto con la misma estructura que el JSON de Usuarios
-       const userToPush = {
-           id: lastUser.id + 1,
-           first_name: userToCreate.first_name,
-           last_name: userToCreate.lastname,
-           email: userToCreate.email,
-           password: passEncriptada,
-           rol: "user",
-           avatar: imageName,
-           phone: userToCreate.phone,
-           user_name: userToCreate.user_name     
-       }
-       
-       console.log(userToPush);
-       
-       //Actualizo la base de datos
-       users.push(userToPush);
-       fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 4));
-
-       return  res.redirect('/users/login');
-    },*/
-
+      },
     processLogin: async function (req, res){
 
         //const userReq = req.body;
@@ -111,7 +72,7 @@ const controller = {
             error=error.mapped()
            return res.render('./users/login',{error})
         }
-    
+
         //Busca el usuario que se está logueando en la Base de Datos
         try {
           let userToLogin = await db.User.findOne({
@@ -119,12 +80,12 @@ const controller = {
             email: req.body.email
             }
             });
-    
+
         //Se procesa el logueo
           if (userToLogin){
             //Verifica que la contraseña sea la misma que la registrada
             let check = bcrypt.compareSync(req.body.password, userToLogin.password);
-                    
+
             if(check){
                 req.session.loggedUser=userToLogin
                 if(req.body.remember){
@@ -132,55 +93,23 @@ const controller = {
                         maxAge: (1000 * 6) * 2
                     })
                 }
-                return res.redirect('/')      
+                return res.redirect('/')
             }else{
                 error={password:{msg:'Password incorrecto'}};
             }
-        
+
             //res.send(userToLogin);
             }else{
                error={email:{msg:'El usuario no existe'}};
-                
+
             }
             return res.render('./users/login',{error})
-            
+
         } catch (err) {
-          console.log(err);      
+          console.log(err);
         }
-    
+
       },
-
-    /*processLogin: (req, res) => {
-
-        const userReq = req.body;
-        let error={msg:''}
-        //Busca el usuario que se está logueando en la Base de Datos
-        const userToLogin = users.find( elem => elem.email == userReq.mail);
-
-        if (userToLogin){
-        //Verifica que la contraseña sea la misma que la registrada
-        let check = bcrypt.compareSync(userReq.psw, userToLogin.password);
-                
-        if(check){
-            req.session.loggedUser=userToLogin
-            if(req.body.remember){
-                res.cookie('userEmail', req.body.mail, {
-                    maxAge: (1000 * 6) * 2
-                })
-            }
-            return res.redirect('/')      
-        }else{
-            error.msg='Password incorrecto';
-        }
-
-        //res.send(userToLogin);
-        }else{
-            error.msg='El usuario no existe';
-            
-        }
-        return res.render('./users/login',{error})
-
-    },*/
 
     logout:(req,res)=>{
         res.clearCookie('userEmail');
